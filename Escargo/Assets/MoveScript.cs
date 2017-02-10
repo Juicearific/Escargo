c4296a4b0284da6926c8c045c8ce228dc71c97d5
jahardner <johannaahardner@gmail.com>
2017-02-10 11:45:25 -0500

    Movement in and works

---
+++ Escargo/Assets/MoveScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    public string upKey = "w";
+    public string downKey = "s";
+    public string leftKey = "a";
+    public string rightKey = "d";
+
+    void FixedUpdate()
+    {
+        Vector2 targetVelocity;
+        if (Input.GetKey(rightKey))
+        {
+            targetVelocity = Vector2.right;
+            //GetComponent<Rigidbody2D>().MovePosition(new Vector2(this.gameObject.transform.position.x + moveSpeed, this.gameObject.transform.position.y));
+        }
+        else if (Input.GetKey(downKey))
+        {
+            targetVelocity = Vector2.down;
+            //GetComponent<Rigidbody2D>().MovePosition(new Vector2(this.gameObject.transform.position.x, this.gameObject.transform.position.y - moveSpeed));
+        }
+        else if (Input.GetKey(leftKey))
+        {
+            targetVelocity = Vector2.left;
+            //GetComponent<Rigidbody2D>().MovePosition(new Vector2(this.gameObject.transform.position.x - moveSpeed, this.gameObject.transform.position.y));
+        }
+        else if (Input.GetKey(upKey))
+        {
+            targetVelocity = Vector2.up;
+            //GetComponent<Rigidbody2D>().MovePosition(new Vector2(this.gameObject.transform.position.x, this.gameObject.transform.position.y + moveSpeed));
+        }
+        else
+        {
+            targetVelocity = Vector2.zero;
+
+        }
+        GetComponent<Rigidbody2D>().velocity = targetVelocity * moveSpeed;
+        GetComponent<Transform>().rotation = new Quaternion(0,0,0,0);
+    }
+}
