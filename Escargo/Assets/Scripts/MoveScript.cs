9ff366a8033b99ec3ca696c6e2a05b648151e581
mfragaini <psychosispanda@gmail.com>
2017-03-29 01:45:51 -0400

    Slime Trail Adaptation - now with transparency!
    Slime trails should adjust to eachother now.

--- Escargo/Assets/Scripts/MoveScript.cs
+++ Escargo/Assets/Scripts/MoveScript.cs
@@ -10,7 +10,14 @@ public class MoveScript : MonoBehaviour
 
     /* Public Variables */
     public GameObject[] otherPlayers;
-    public Object slimeSprite;
+    public GameObject[] slimeSprites = new GameObject[6];
+    // 0 - Dottrail
+    // 1 - Singletrail
+    // 2 - Sidetrail
+    // 3 - Cornertrail
+    // 4 - Tritrail
+    // 5 - Quadtrail
+    public GameObject[,] slimeObjGrid = new GameObject[SnaillingScript.WIDTH, SnaillingScript.HEIGHT];
     public Text slimeBox;
     public string upKey = "w";
     public string downKey = "s";
@@ -113,11 +120,140 @@ public class MoveScript : MonoBehaviour
             if (slimeAmt >= 0)
             {
                 GetComponent<PlayerScript>().slime = slimeAmt;
-                Object.Instantiate(slimeSprite, new Vector3(((float)gridX) + .5f, ((float)gridY) + .5f, 0), Quaternion.identity);
+                slimeObjGrid[gridX, gridY] = GameObject.Instantiate(slimeSprites[0], new Vector3(((float)gridX) + .5f, ((float)gridY) + .5f, 0), Quaternion.identity);
+                updateSlime(gridX, gridY, 0);
                 //GetComponent<PlayerScript>().slider.value = GetComponent<PlayerScript>().slime;
             }
         }
 	}
+
+    void updateSlime(int x, int y, int depth)
+    {
+        if (depth < 2) // Should only need to adjust up to two slimes out - but, we can adjust this if we find the need to affect more later. 
+        {
+            bool up = false;
+            bool left = false;
+            bool right = false;
+            bool down = false;
+            int id = GetComponent<SnaillingScript>().playerID;
+            int[,] grid = GetComponent<SnaillingScript>().slimeGrid;
+            // Set up the adjacencies
+            if (x - 1 >= 0 && grid[x - 1, y] == id)
+            {
+                left = true;
+                updateSlime(x - 1, y, depth + 1);
+            }
+            if (x + 1 <= SnaillingScript.WIDTH && grid[x + 1, y] == id)
+            {
+                right = true;
+                updateSlime(x + 1, y, depth + 1);
+            }
+            if (y - 1 >= 0 && grid[x, y - 1] == id)
+            {
+                down = true;
+                updateSlime(x, y - 1, depth + 1);
+            }
+            if (y + 1 <= SnaillingScript.HEIGHT && grid[x, y + 1] == id)
+            {
+                up = true;
+                updateSlime(x, y + 1, depth + 1);
+            }
+
+            // Adjust the sprites
+            // For Quad:
+            if (left && right && up && down)
+            {
+                Destroy(slimeObjGrid[x, y]);
+                slimeObjGrid[x,y] = GameObject.Instantiate(slimeSprites[5], new Vector3(((float)x) + .5f, ((float)y) + .5f, 0), Quaternion.identity);
+            }
+            // For Tri:
+            else if (left && down && right) // Default
+            {
+                Destroy(slimeObjGrid[x, y]);
+                slimeObjGrid[x, y] = GameObject.Instantiate(slimeSprites[4], new Vector3(((float)x) + .5f, ((float)y) + .5f, 0), Quaternion.identity);
+            }
+            else if (up && right && down) // 90 deg rotation
+            {
+                Destroy(slimeObjGrid[x, y]);
+                slimeObjGrid[x, y] = GameObject.Instantiate(slimeSprites[4], new Vector3(((float)x) + .5f, ((float)y) + .5f, 0), Quaternion.identity);
+                slimeObjGrid[x, y].transform.Rotate(0, 0, 90);
+            }
+            else if (left && up && right) // 180 deg rotation
+            {
+                Destroy(slimeObjGrid[x, y]);
+                slimeObjGrid[x, y] = GameObject.Instantiate(slimeSprites[4], new Vector3(((float)x) + .5f, ((float)y) + .5f, 0), Quaternion.identity);
+                slimeObjGrid[x, y].transform.Rotate(0, 0, 180);
+            }
+            else if (down && left && up) // 270 deg rotation
+            {
+                Destroy(slimeObjGrid[x, y]);
+                slimeObjGrid[x, y] = GameObject.Instantiate(slimeSprites[4], new Vector3(((float)x) + .5f, ((float)y) + .5f, 0), Quaternion.identity);
+                slimeObjGrid[x, y].transform.Rotate(0, 0, 270);
+            }
+            // For Corner:
+            else if (left && down) // Default
+            {
+                Destroy(slimeObjGrid[x, y]);
+                slimeObjGrid[x, y] = GameObject.Instantiate(slimeSprites[3], new Vector3(((float)x) + .5f, ((float)y) + .5f, 0), Quaternion.identity);
+            }
+            else if (down && right) // 90 deg rotation
+            {
+                Destroy(slimeObjGrid[x, y]);
+                slimeObjGrid[x, y] = GameObject.Instantiate(slimeSprites[3], new Vector3(((float)x) + .5f, ((float)y) + .5f, 0), Quaternion.identity);
+                slimeObjGrid[x, y].transform.Rotate(0, 0, 90);
+            }
+            else if (up && right) // 180 deg rotation
+            {
+                Destroy(slimeObjGrid[x, y]);
+                slimeObjGrid[x, y] = GameObject.Instantiate(slimeSprites[3], new Vector3(((float)x) + .5f, ((float)y) + .5f, 0), Quaternion.identity);
+                slimeObjGrid[x, y].transform.Rotate(0, 0, 180);
+            }
+            else if (left && up) // 270 deg rotation
+            {
+                Destroy(slimeObjGrid[x, y]);
+                slimeObjGrid[x, y] = GameObject.Instantiate(slimeSprites[3], new Vector3(((float)x) + .5f, ((float)y) + .5f, 0), Quaternion.identity);
+                slimeObjGrid[x, y].transform.Rotate(0, 0, 270);
+            }
+            // For Sidetrail
+            else if (left && right) // Default
+            {
+                Destroy(slimeObjGrid[x, y]);
+                slimeObjGrid[x, y] = GameObject.Instantiate(slimeSprites[2], new Vector3(((float)x) + .5f, ((float)y) + .5f, 0), Quaternion.identity);
+            }
+            else if (up && down) // 90 deg rotation
+            {
+                Destroy(slimeObjGrid[x, y]);
+                slimeObjGrid[x, y] = GameObject.Instantiate(slimeSprites[2], new Vector3(((float)x) + .5f, ((float)y) + .5f, 0), Quaternion.identity);
+                slimeObjGrid[x, y].transform.Rotate(0, 0, 90);
+            }
+            // For Singletrail
+            else if (left) // Default
+            {
+                Destroy(slimeObjGrid[x, y]);
+                slimeObjGrid[x, y] = GameObject.Instantiate(slimeSprites[1], new Vector3(((float)x) + .5f, ((float)y) + .5f, 0), Quaternion.identity);
+            }
+            else if (down) // 90 deg rotation
+            {
+                Destroy(slimeObjGrid[x, y]);
+                slimeObjGrid[x, y] = GameObject.Instantiate(slimeSprites[1], new Vector3(((float)x) + .5f, ((float)y) + .5f, 0), Quaternion.identity);
+                slimeObjGrid[x, y].transform.Rotate(0, 0, 90);
+            }
+            else if (right) // 180 deg rotation
+            {
+                Destroy(slimeObjGrid[x, y]);
+                slimeObjGrid[x, y] = GameObject.Instantiate(slimeSprites[1], new Vector3(((float)x) + .5f, ((float)y) + .5f, 0), Quaternion.identity);
+                slimeObjGrid[x, y].transform.Rotate(0, 0, 180);
+            }
+            else if (up) // 270 deg rotation
+            {
+                Destroy(slimeObjGrid[x, y]);
+                slimeObjGrid[x, y] = GameObject.Instantiate(slimeSprites[1], new Vector3(((float)x) + .5f, ((float)y) + .5f, 0), Quaternion.identity);
+                slimeObjGrid[x, y].transform.Rotate(0, 0, 270);
+            }
+
+        }
+
+    }
 
 	void OnCollisionEnter2D(Collision2D collision) {
 		//Shelling - Display Animation for Shell Collision here.
