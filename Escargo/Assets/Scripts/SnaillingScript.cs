6adc6e02c692699e3d66439a5744db2aa9d32243
jahardner <johannaahardner@gmail.com>
2017-04-08 17:36:49 -0400

    should be better, but still doens't work

--- Escargo/Assets/Scripts/SnaillingScript.cs
+++ Escargo/Assets/Scripts/SnaillingScript.cs
@@ -41,13 +41,13 @@ public class SnaillingScript : MonoBehaviour {
     public const int WIDTH = 50;
 
     /* Public Variables */
-    public static int[,] slimeGrid = new int[WIDTH, HEIGHT];
-    public List<KeyValuePair<int, int>> closestNode = new List<KeyValuePair<int, int>>();
-    KeyValuePair<int, int>[] lastMove = new KeyValuePair<int, int>[NUM_SNAILLINGS];
+    public static int[,] slimeGrid;
+    public List<KeyValuePair<int, int>> closestNode;
+    KeyValuePair<int, int>[] lastMove;
     public Object snaillingsSprite;
-    public GameObject[] snaillings = new GameObject[NUM_SNAILLINGS];
+    public GameObject[] snaillings;
     public int currentSnail = 0;
-    public Stack<Vector3>[] snaillingsMove = new Stack<Vector3>[NUM_SNAILLINGS];
+    public Stack<Vector3>[] snaillingsMove;
 
     /* Private Variables */
     private float spawnTimer = 5.0f;
@@ -60,13 +60,22 @@ public class SnaillingScript : MonoBehaviour {
 
     void Start()
     {
-		playerID = GetComponent<PlayerScript> ().playerID;
+        slimeGrid = new int[WIDTH, HEIGHT];
+        closestNode = new List<KeyValuePair<int, int>>();
+        lastMove = new KeyValuePair<int, int>[NUM_SNAILLINGS];
+        snaillings = new GameObject[NUM_SNAILLINGS];
+        snaillingsMove = new Stack<Vector3>[NUM_SNAILLINGS];
+
+        playerID = GetComponent<PlayerScript> ().playerID;
         snailStartX = GetComponent<Transform>().position.x;
         snailStartY = GetComponent<Transform>().position.y;
         for (int i = 0; i < NUM_SNAILLINGS; i++)
         {
             snaillings[i] = (GameObject)Object.Instantiate(snaillingsSprite, new Vector3(snailStartX, snailStartY, -500), Quaternion.identity);
-            snaillingsMove[i] = new Stack<Vector3>();
+            lock (snaillingsMove)
+            {
+                snaillingsMove[i] = new Stack<Vector3>();
+            }
             lastMove[i] = new KeyValuePair<int, int>(-10,-10);
         }
     }
@@ -100,22 +109,36 @@ public class SnaillingScript : MonoBehaviour {
                             deadend = 0;
                         }
                         // no simple path found, run AI
-                        snaillingsMove[i].Clear();
+                        lock (snaillingsMove)
+                        {
+                            snaillingsMove[i].Clear();
+                        }
                         KeyValuePair<int, int> n = closestNode[0];
                         if (!(oX == n.Key && oY == n.Value))
                         {
-                            Debug.Log("Pathing from " + oX + "," + oY + " to " + n.Key + "," + n.Value);
+                            //Debug.Log("Pathing from " + oX + "," + oY + " to " + n.Key + "," + n.Value);
                             snailPathThread = new Thread(() => aStar(i, oX, oY, n.Key, n.Value));
                             snailPathThread.Start();
                             while (!snailPathThread.IsAlive);
                         }
                     }
-                    Debug.Log(snaillingsMove[i].Count);
-                    if (snaillingsMove[i].Count > 0)
+                    //Debug.Log(snaillingsMove[i].Count);
+
+                    int count;
+                    lock (snaillingsMove)
+                    {
+                        count = snaillingsMove[i].Count;
+                    }
+
+                    if (count > 0)
                     {
                         lastMove[i] = new KeyValuePair<int, int>(oX, oY);
-                        Vector3 newPos = snaillingsMove[i].Pop();
-                        Debug.Log(newPos);
+                        Vector3 newPos;
+                        lock (snaillingsMove)
+                        {
+                            newPos = snaillingsMove[i].Pop();
+                        }
+                        //Debug.Log(newPos);
                         snaillings[i].transform.position = newPos; // actually make move
                         /*Lerp:
                             * Vector3 origPos = snaillings [i].transform.position;
@@ -170,11 +193,13 @@ public class SnaillingScript : MonoBehaviour {
             }
             dirs++;
         }
-        //Debug.Log("lastMove: " + lastMove[sID].Key + "," + lastMove[sID].Value);
         if (dirs == 2)
         {
             // only one path (aka only forward and backwards)
-            snaillingsMove[sID].Push(new Vector3(nextMove.Key + .5f, nextMove.Value + .5f, 0));
+            lock (snaillingsMove)
+            {
+                snaillingsMove[sID].Push(new Vector3(nextMove.Key + .5f, nextMove.Value + .5f, 0));
+            }
             deadend = 0;
             return true;
         } else if (dirs == 1)
@@ -268,7 +293,10 @@ public class SnaillingScript : MonoBehaviour {
         }
         while (dist.p_x != -1 || dist.p_y != -1)
         {
-            snaillingsMove[sID].Push(new Vector3(dist.x + .5f, dist.y + .5f, 0));
+            lock (snaillingsMove)
+            {
+                snaillingsMove[sID].Push(new Vector3(dist.x + .5f, dist.y + .5f, 0));
+            }
             dist.x = dist.p_x;
             dist.y = dist.p_y;
             for (int i = 0; i < closed.Count; i++)
@@ -280,7 +308,7 @@ public class SnaillingScript : MonoBehaviour {
                 }
             }
         }
-        Debug.Log("count at end fo astar: " +snaillingsMove[sID].Count);
+        //Debug.Log("count at end fo astar: " +snaillingsMove[sID].Count);
     }
 
     bool searchList(List<Node> l, Node n)
