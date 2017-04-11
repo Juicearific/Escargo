3a0cc5b121aef4f33ee00e3ef450771b5ce46f38
mfragaini <psychosispanda@gmail.com>
2017-04-11 18:59:57 -0400

    Character Select rev 1
    Mostly working. Left arrows don't work (I don't know why). Characters
    are not actually spawned in based on selections, yet, but the player
    names in the static script areupdated approriately.

---
+++ Escargo/Assets/Scripts/OptionsStaticScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsStaticScript : MonoBehaviour
+{
+    public static KeyCode[,] controls = new KeyCode[4, 6] { { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.E, KeyCode.Q } ,
+                                                            {KeyCode.F, KeyCode.C, KeyCode.B, KeyCode.V, KeyCode.G, KeyCode.R},
+                                                            {KeyCode.U, KeyCode.H, KeyCode.K, KeyCode.J, KeyCode.I, KeyCode.Y},
+                                                            {KeyCode.L, KeyCode.Comma, KeyCode.Slash, KeyCode.Period, KeyCode.Semicolon, KeyCode.O } };
+    // [x,0] = Up
+    // [x,1] = Left
+    // [x,2] = Right
+    // [x,3] = Down
+    // [x,4] = Slime Toggle
+    // [x,5] = Map
+
+    // Current players: Can be 'pierre', 'kenta', 'jim', 'bertha', or 'n/a'
+    public static string p1Name = "pierre";
+    public static string p2Name = "kenta";
+    public static string p3Name = "n/a";
+    public static string p4Name = "n/a";
+}
---
+++ Escargo/Assets/Scripts/SelectScript.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SelectScript : MonoBehaviour {
+
+    public Sprite[] imgs = new Sprite[5];
+    public Image p1Img;
+    public int p1Index;
+    public Image p2Img;
+    public int p2Index;
+    public Image p3Img;
+    public int p3Index;
+    public Image p4Img;
+    public int p4Index;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void p1Right()
+    {
+        p1Index++;
+        if (p1Index > 4)
+        {
+            p1Index = 0;
+        }
+        p1Img.sprite = imgs[p1Index];
+        OptionsStaticScript.p3Name = updateSprite(p3Index);
+    }
+
+    public void p1Left()
+    {
+        p1Index--;
+        if (p1Index < 0)
+        {
+            p1Index = 4;
+        }
+        p1Img.sprite = imgs[p1Index];
+        OptionsStaticScript.p3Name = updateSprite(p3Index);
+    }
+
+    public void p2Right()
+    {
+        p2Index++;
+        if (p2Index > 4)
+        {
+            p2Index = 0;
+        }
+        p2Img.sprite = imgs[p2Index];
+        OptionsStaticScript.p3Name = updateSprite(p3Index);
+    }
+
+    public void p2Left()
+    {
+        p2Index--;
+        if (p2Index < 0)
+        {
+            p2Index = 4;
+        }
+        p2Img.sprite = imgs[p2Index];
+        OptionsStaticScript.p3Name = updateSprite(p3Index);
+    }
+
+    public void p3Right()
+    {
+        p3Index++;
+        if (p3Index > 4)
+        {
+            p3Index = 0;
+        }
+        p3Img.sprite = imgs[p3Index];
+        OptionsStaticScript.p3Name = updateSprite(p3Index);
+    }
+
+    public void p3Left()
+    {
+        p3Index--;
+        if (p3Index < 0)
+        {
+            p3Index = 4;
+        }
+        p3Img.sprite = imgs[p3Index];
+        OptionsStaticScript.p3Name = updateSprite(p3Index);
+    }
+
+    public void p4Right()
+    {
+        p4Index++;
+        if (p4Index > 4)
+        {
+            p4Index = 0;
+        }
+        p4Img.sprite = imgs[p4Index];
+        OptionsStaticScript.p4Name = updateSprite(p4Index);
+    }
+
+    public void p4Left()
+    {
+        p4Index--;
+        if (p4Index < 0)
+        {
+            p4Index = 4;
+        }
+        p4Img.sprite = imgs[p4Index];
+        OptionsStaticScript.p4Name = updateSprite(p4Index);
+    }
+
+    public string updateSprite(int index)
+    {
+        if (index == 0)
+        {
+            return "n/a";
+        }
+        else if(index == 1)
+        {
+            return "pierre";
+        }
+        else if(index == 2)
+        {
+            return "kenta";
+        }
+        else if (index == 3)
+        {
+            return "bertha";
+        }
+        else // (index == 4)
+        {
+            return "liljim";
+        }
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("Demo");
+    }
+}
