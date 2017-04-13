d0c4026c4f2cdac08a8a1db946b8be140b032ac1
Daria Cook <nekonohimitsu@hotmail.com>
2017-04-13 03:05:12 -0400

    Basics-In
    Snails are generated at start now.
    Need animations for Big Bertha and Lil Jim to work
    SplitScreen sizes are a little bit off right now.
    AI Crashes on four players rather quickly.

--- Escargo/Assets/Scripts/GlobalScript.cs
+++ Escargo/Assets/Scripts/GlobalScript.cs
@@ -4,55 +4,135 @@ using UnityEngine;
 
 public class GlobalScript : MonoBehaviour {
 	public const int NUM_PLAYERS = 4;
+	private const int PIERRE = 0;
+	private const int KENTA = 1;
+	private const int LILJIM = 2;
+	private const int BIGBERTHA = 3;
+	private Color[] colors = { Color.blue, Color.red, Color.yellow, Color.black };
+	public GameObject[] snailPrefabs = new GameObject[NUM_PLAYERS];
 	public GameObject[] snaillingDisplayPrefabs = new GameObject[NUM_PLAYERS];
-	// 0 => Pierre
-	// 1 => Kenta
-	// 2 => LilJim
-	// 3 => BigBertha
-	private GameObject[] snaillingPanels = new GameObject[NUM_PLAYERS];
-	private string[] snailTypes = new string[NUM_PLAYERS];
-	private Color[] snailColors = new Color[NUM_PLAYERS];
+	public GameObject[] player1SlimeSprites = new GameObject[MoveScript.NUM_SLIME_SPRITES];
+	public GameObject[] player2SlimeSprites = new GameObject[MoveScript.NUM_SLIME_SPRITES];
+	public GameObject[] player3SlimeSprites = new GameObject[MoveScript.NUM_SLIME_SPRITES];
+	public GameObject[] player4SlimeSprites = new GameObject[MoveScript.NUM_SLIME_SPRITES];
+	private GameObject[][] specificPlayerSprites = new GameObject[NUM_PLAYERS][];
+	private static PlayerScript[] players = new PlayerScript[NUM_PLAYERS];
+	private Dictionary<GameObject, GameObject[]> playersSnaillingDisplays = new Dictionary<GameObject, GameObject[]>();
+	private float startX = 1.5f;
+	private float startY = 24f;
+	private const float START_DISTANCE = 6.0f;
+	public static bool Ready = false;
+
+
+	public void Start() {
+		specificPlayerSprites [0] = player1SlimeSprites;
+		specificPlayerSprites [1] = player2SlimeSprites;
+		specificPlayerSprites [2] = player3SlimeSprites;
+		specificPlayerSprites [3] = player4SlimeSprites;
+		setupPlayers();
+		setupCameras ();
+		setupPanels ();
+		Ready = true;
+		}
 
 	public void changeDisplay(int playerID, int newSnaillingCount) {
 		for (int i = 0; i < NUM_PLAYERS; i++) {
-			if (snaillingPanels [i] != null) {
-				snaillingPanels [i].GetComponentInChildren<UnityEngine.UI.Text> ().text = newSnaillingCount.ToString ()
+			if (players [i] != null) {
+				playersSnaillingDisplays[players[i].snaillingPanel][playerID - 1].GetComponentInChildren<UnityEngine.UI.Text> ().text = newSnaillingCount.ToString ()
 				+ "/" + SnaillingScript.NUM_SNAILLINGS.ToString ();
-				snaillingPanels [i].GetComponentInChildren<UnityEngine.UI.Slider> ().value = newSnaillingCount;
+				playersSnaillingDisplays[players[i].snaillingPanel][playerID - 1].GetComponentInChildren<UnityEngine.UI.Slider> ().value = newSnaillingCount;
 			}
 		}
 	}
 
-	public void setSnaillingDisplay(int playerID, string snailType, GameObject snaillingPanel, Color snailColor) {
-		snaillingPanels [playerID - 1] = snaillingPanel;
-		snailTypes [playerID - 1] = snailType;
-		snailColors [playerID - 1] = snailColor;
-		setupPanels ();
+	private void setupPlayers() {
+		setupPlayer (OptionsStaticScript.p1Name, 1);
+		setupPlayer (OptionsStaticScript.p2Name, 2);
+		setupPlayer (OptionsStaticScript.p3Name, 3);
+		setupPlayer (OptionsStaticScript.p4Name, 4);
 	}
 
-	public void setupPanels() {
+	private void setupPlayer(string name, int ID) {
+		GameObject player = null;
+		switch (name) {
+		case "n/a":
+			break;//No player selected
+		case "pierre":
+			player = Instantiate (snailPrefabs [PIERRE]);
+			break;
+		case "kenta":
+			player = Instantiate (snailPrefabs [KENTA]);
+			break;
+		case "liljim":
+			player = Instantiate (snailPrefabs [LILJIM]);
+			break;
+		case "bertha":
+			player = Instantiate (snailPrefabs [BIGBERTHA]);
+			break;
+		}
+		if (player != null) {
+			PlayerScript script = player.GetComponent<PlayerScript> ();
+			players [ID - 1] = script;
+			script.playerID = ID;
+			script.playerColor = colors [ID - 1];
+			player.GetComponent<SnaillingScript> ().playerID = ID;
+			player.GetComponent<MoveScript> ().setSlimeSprites (specificPlayerSprites[ID - 1]);
+			player.GetComponent<MoveScript> ().updateColors ();
+			player.transform.localPosition = new Vector2 (startX, startY - (START_DISTANCE * (ID - 1)));
+		} else {
+			players [ID - 1] = null;
+		}
+	}
+
+	private void setupCameras() {
+		int numCameras = 0;
+		SplitscreenScript script = GetComponent<SplitscreenScript> ();
+		if (players [0] != null) {
+			script.cam1 = players [0].gameObject.GetComponentInChildren<Camera> ();
+			numCameras++;
+		}
+		if (players [1] != null) {
+			script.cam2 = players [1].gameObject.GetComponentInChildren<Camera> ();
+			numCameras++;
+		}
+		if (players [2] != null) {
+			script.cam3 = players [2].gameObject.GetComponentInChildren<Camera> ();
+			numCameras++;
+		}
+		if (players [3] != null) {
+			script.cam4 = players [3].gameObject.GetComponentInChildren<Camera> ();
+			numCameras++;
+		}
+		SplitscreenScript.numCameras = numCameras;
+		script.initCameras ();
+	}
+
+	private void setupPanels() {
 		for (int i = 0; i < NUM_PLAYERS; i++) {
-			if (snaillingPanels [i] != null) {
-				foreach (Transform child in snaillingPanels[i].transform)
-					GameObject.Destroy (child.gameObject);
+			if (players [i] != null) {
+				GameObject[] prefabs = new GameObject[NUM_PLAYERS];
 				for (int j = 0; j < NUM_PLAYERS; j++) {
-					if (snailTypes [j] != null) {
+					if (players[j] != null) {
 						GameObject prefab;
-						if (snailTypes[j] == "pierre") {
-							prefab = Instantiate (snaillingDisplayPrefabs [0]);
-						} else if (snailTypes[j] == "kenta") {
-							prefab = Instantiate (snaillingDisplayPrefabs [1]);
-						} else if (snailTypes[j] == "liljim") {
-							prefab = Instantiate (snaillingDisplayPrefabs [2]);
+						if (players[j].snailType == "pierre") {
+							prefab = Instantiate (snaillingDisplayPrefabs [PIERRE]);
+						} else if (players[j].snailType == "kenta") {
+							prefab = Instantiate (snaillingDisplayPrefabs [KENTA]);
+						} else if (players[j].snailType == "liljim") {
+							prefab = Instantiate (snaillingDisplayPrefabs [LILJIM]);
 						} else {//Assume it's bertha.
-							prefab = Instantiate (snaillingDisplayPrefabs [3]);
+							prefab = Instantiate (snaillingDisplayPrefabs [BIGBERTHA]);
 						}
-						prefab.transform.SetParent (snaillingPanels [i].transform, false);
-						prefab.GetComponentInChildren<UnityEngine.UI.Slider> ().fillRect.GetComponent<UnityEngine.UI.Image> ().color = snailColors [j];
+						prefab.transform.SetParent (players [i].snaillingPanel.transform, false);
+						prefab.GetComponentInChildren<UnityEngine.UI.Slider> ().fillRect.GetComponent<UnityEngine.UI.Image> ().color = players [j].playerColor;
+						prefabs [j] = prefab;
 					}
 				}
-
+				playersSnaillingDisplays.Add (players [i].snaillingPanel, prefabs);
 			}
 		}
+	}
+	public static PlayerScript[] getPlayers() {
+		return players;
 	}
 }
--- Escargo/Assets/Scripts/SnaillingScript.cs
+++ Escargo/Assets/Scripts/SnaillingScript.cs
@@ -48,13 +48,14 @@ public class SnaillingScript : MonoBehaviour {
     public GameObject[] snaillings;
     public int currentSnail = 0;
     public Stack<Vector3>[] snaillingsMove;
+	public bool Ready = false;
+	public int playerID;
 
     /* Private Variables */
     private float spawnTimer = 5.0f;
     private float moveTimer = 0.0f;
     private float snailStartX;
     private float snailStartY;
-    private int playerID;
     private int deadend = 0;
     Thread snailPathThread;
 
@@ -65,8 +66,6 @@ public class SnaillingScript : MonoBehaviour {
         lastMove = new KeyValuePair<int, int>[NUM_SNAILLINGS];
         snaillings = new GameObject[NUM_SNAILLINGS];
         snaillingsMove = new Stack<Vector3>[NUM_SNAILLINGS];
-
-        playerID = GetComponent<PlayerScript> ().playerID;
         snailStartX = GetComponent<Transform>().position.x;
         snailStartY = GetComponent<Transform>().position.y;
         for (int i = 0; i < NUM_SNAILLINGS; i++)
@@ -78,91 +77,82 @@ public class SnaillingScript : MonoBehaviour {
             }
             lastMove[i] = new KeyValuePair<int, int>(-10,-10);
         }
+		Ready = true;
     }
 
     void FixedUpdate () {
-        /* snaillings spawn */
-        spawnTimer += Time.deltaTime;
-        if (spawnTimer >= 10f && currentSnail < NUM_SNAILLINGS)
-        {
-            spawnTimer = 0f;
-            snaillings[currentSnail].transform.position = new Vector3(snaillings[currentSnail].transform.position.x,
-                snaillings[currentSnail].transform.position.y, 0);
-            snaillings[currentSnail].tag = "P" + playerID.ToString() + "Snailling";
-            currentSnail++;
-        }
-        /* snailings move */
-        moveTimer += Time.deltaTime;
-        if (moveTimer >= 1.5f)
-        {
-            moveTimer = 0f;
-            for (int i = 0; i < currentSnail; i++)
-            {
-                if (snaillings[i] != null)
-                {
-                    int oX = (int)snaillings[i].transform.position.x;
-                    int oY = (int)snaillings[i].transform.position.y;
-                    bool simple = findSimplePath(i, oX, oY);
-                    if (!simple && (deadend <= 1 || deadend > 5) && (snailPathThread == null || !snailPathThread.IsAlive))
-                    {
-                        if (deadend > 5) {
-                            deadend = 0;
-                        }
-                        // no simple path found, run AI
-                        lock (snaillingsMove)
-                        {
-                            snaillingsMove[i].Clear();
-                        }
-                        KeyValuePair<int, int> n = closestNode[0];
-                        if (!(oX == n.Key && oY == n.Value))
-                        {
-                            //Debug.Log("Pathing from " + oX + "," + oY + " to " + n.Key + "," + n.Value);
-                            snailPathThread = new Thread(() => aStar(i, oX, oY, n.Key, n.Value));
-                            snailPathThread.Start();
-                            while (!snailPathThread.IsAlive);
-                            while (snailPathThread.IsAlive);
-                        }
-                    }
-                    //Debug.Log(snaillingsMove[i].Count);
+		if (GlobalScript.Ready) {
+			/* snaillings spawn */
+			spawnTimer += Time.deltaTime;
+			if (spawnTimer >= 10f && currentSnail < NUM_SNAILLINGS) {
+				spawnTimer = 0f;
+				snaillings [currentSnail].transform.position = new Vector3 (snaillings [currentSnail].transform.position.x,
+					snaillings [currentSnail].transform.position.y, 0);
+				snaillings [currentSnail].tag = "P" + GetComponent<PlayerScript> ().playerID.ToString () + "Snailling";
+				currentSnail++;
+			}
+			/* snailings move */
+			moveTimer += Time.deltaTime;
+			if (moveTimer >= 1.5f) {
+				moveTimer = 0f;
+				for (int i = 0; i < currentSnail; i++) {
+					if (snaillings [i] != null) {
+						int oX = (int)snaillings [i].transform.position.x;
+						int oY = (int)snaillings [i].transform.position.y;
+						bool simple = findSimplePath (i, oX, oY);
+						if (!simple && (deadend <= 1 || deadend > 5) && (snailPathThread == null || !snailPathThread.IsAlive)) {
+							if (deadend > 5) {
+								deadend = 0;
+							}
+							// no simple path found, run AI
+							lock (snaillingsMove) {
+								snaillingsMove [i].Clear ();
+							}
+							KeyValuePair<int, int> n = closestNode [0];
+							if (!(oX == n.Key && oY == n.Value)) {
+								snailPathThread = new Thread (() => aStar (i, oX, oY, n.Key, n.Value));
+								snailPathThread.Start ();
+								while (!snailPathThread.IsAlive)
+									;
+								while (snailPathThread.IsAlive)
+									;
+							}
+						}
 
-                    int count;
-                    lock (snaillingsMove)
-                    {
-                        count = snaillingsMove[i].Count;
-                    }
+						int count;
+						lock (snaillingsMove) {
+							count = snaillingsMove [i].Count;
+						}
 
-                    if (count > 0)
-                    {
-                        lastMove[i] = new KeyValuePair<int, int>(oX, oY);
-                        Vector3 newPos;
-                        lock (snaillingsMove)
-                        {
-                            newPos = snaillingsMove[i].Pop();
-                        }
-                        //Debug.Log(newPos);
-                        snaillings[i].transform.position = newPos; // actually make move
-                        /*Lerp:
+						if (count > 0) {
+							lastMove [i] = new KeyValuePair<int, int> (oX, oY);
+							Vector3 newPos;
+							lock (snaillingsMove) {
+								newPos = snaillingsMove [i].Pop ();
+							}
+							snaillings [i].transform.position = newPos; // actually make move
+							/*Lerp:
                             * Vector3 origPos = snaillings [i].transform.position;
                         float speed = 15f;
                         float smooth = 1.0f - Mathf.Pow (0.5f, Time.deltaTime * speed);
                         snaillings [i].transform.position = Vector3.Slerp (origPos, newPos, smooth);
                         */
-                    }
-                }
-            }
-        }
+						}
+					}
+				}
+			}
+		}
     }
 
     bool findSimplePath(int sID, int gridX, int gridY)
     {
-        int playerID = GetComponent<PlayerScript>().playerID;
         int dirs = 0;
         if (gridX < 2)
         {
             dirs++; // so it looks like there's two directions from start
         }
         KeyValuePair<int, int> nextMove = new KeyValuePair<int, int>();
-        if (gridX + 1 < WIDTH && slimeGrid[gridX + 1, gridY] == playerID)
+		if (gridX + 1 < WIDTH && slimeGrid[gridX + 1, gridY] == playerID)
         {
             if (!(lastMove[sID].Key == gridX + 1 && lastMove[sID].Value == gridY))
             {
@@ -170,7 +160,7 @@ public class SnaillingScript : MonoBehaviour {
             }
             dirs++;
         }
-        if (gridY + 1 < HEIGHT && slimeGrid[gridX, gridY + 1] == playerID)
+		if (gridY + 1 < HEIGHT && slimeGrid[gridX, gridY + 1] == playerID)
         {
             if (!(lastMove[sID].Key == gridX && lastMove[sID].Value == gridY + 1))
             {
@@ -186,7 +176,7 @@ public class SnaillingScript : MonoBehaviour {
             }
             dirs++;
         }
-        if (gridX - 1 >= 0 && slimeGrid[gridX - 1, gridY] == playerID)
+		if (gridX - 1 >= 0 && slimeGrid[gridX - 1, gridY] == playerID)
         {
             if (!(lastMove[sID].Key == gridX - 1 && lastMove[sID].Value == gridY))
             {
@@ -244,15 +234,15 @@ public class SnaillingScript : MonoBehaviour {
             { // check for node below
                 succ.Add(new Node(q.x + 1, q.y, q.x, q.y, q.g, distX, distY));
             }
-            if (q.x - 1 >= 0 && slimeGrid[q.x - 1,q.y] == playerID)
+			if (q.x - 1 >= 0 && slimeGrid[q.x - 1,q.y] == playerID)
             { // check for node above
                 succ.Add(new Node(q.x - 1, q.y, q.x, q.y, q.g, distX, distY));
             }
-            if (q.y + 1 < HEIGHT && slimeGrid[q.x,q.y + 1] == playerID)
+			if (q.y + 1 < HEIGHT && slimeGrid[q.x,q.y + 1] == playerID)
             { // check for node right
                 succ.Add(new Node(q.x, q.y + 1, q.x, q.y, q.g, distX, distY));
             }
-            if (q.y - 1 >= 0 && slimeGrid[q.x,q.y - 1] == playerID)
+			if (q.y - 1 >= 0 && slimeGrid[q.x,q.y - 1] == playerID)
             { // check for node left
                 succ.Add(new Node(q.x, q.y - 1, q.x, q.y, q.g, distX, distY));
             }
